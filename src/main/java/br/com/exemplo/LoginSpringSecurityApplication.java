47c7b7034f621fcb6697f65a1de5fce02279ce71
Marcos Araujo <rtmaraujo@gmail.com>
2018-07-04 14:29:48 -0300

    Initial commit

---
+++ src/main/java/br/com/exemplo/LoginSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package br.com.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringSecurityApplication.class, args);
+	}
+}
