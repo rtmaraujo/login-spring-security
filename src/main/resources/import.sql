a56f36c52a0e8b28e65925d8803b9f8160b38089
Marcos Araujo <rtmaraujo@gmail.com>
2018-07-05 01:04:02 -0300

    alteração no arquivo properties e inserção de dados na tabela cliente, carro e usuarios_roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,11 @@ insert into role(id, nome_role) values(null, 'ROLE_USER');
 
 insert into usuarios_roles(usuario_id, role_id) values (1L, 1L);
 insert into usuarios_roles(usuario_id, role_id) values (2L, 2L);
+
+insert into cliente (id, nome, idade) values (null, 'Carlos Eduardo', 23);
+insert into cliente (id, nome, idade) values (null, 'Roberto Silva', 28);
+insert into cliente (id, nome, idade) values (null, 'Joaquim Silva', 55);
+
+insert into carro (id, nome_modelo, ano) values (null, 'Uno Fiat', 2010);
+insert into carro (id, nome_modelo, ano) values (null, 'Onix Chevrolet', 2015);
+insert into carro (id, nome_modelo, ano) values (null, 'Gol Volkswagen', 2011);
